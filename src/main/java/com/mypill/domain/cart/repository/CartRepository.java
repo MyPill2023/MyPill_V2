cb645413f8ba99546667ee083f39ab239c7068ca
jny0 <pjy100402@naver.com>
2023-10-08 15:46:19 +0900

    Refactor : 장바구니 페치조인 쿼리 삭제

--- src/main/java/com/mypill/domain/cart/repository/CartRepository.java
+++ src/main/java/com/mypill/domain/cart/repository/CartRepository.java
@@ -1,12 +1,8 @@
 package com.mypill.domain.cart.repository;
 
 import com.mypill.domain.cart.entity.Cart;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface CartRepository extends JpaRepository<Cart, Long>, CartRepositoryCustom {
-    @EntityGraph(attributePaths = {"cartProducts", "cartProducts.product", "cartProducts.product.seller"})
-    Optional<Cart> findByMemberId(Long memberId);
+
 }
