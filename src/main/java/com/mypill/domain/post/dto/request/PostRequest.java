237b583f4d8894b2c4cc903a9d98188b58794d24
jny0 <pjy100402@naver.com>
2023-10-12 11:50:53 +0900

    Refactor : PostFactory 구현 및 적용

--- src/main/java/com/mypill/domain/post/dto/request/PostRequest.java
+++ src/main/java/com/mypill/domain/post/dto/request/PostRequest.java
@@ -2,10 +2,14 @@ package com.mypill.domain.post.dto.request;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PostRequest {
     @NotNull
     @Size(max = 50)
